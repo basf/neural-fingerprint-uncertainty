b2e8b0e13fa51a4ec56cbf4cf42453a620de12c4
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-03-20 23:13:31 +0100

    remove unused file

--- scripts/get_endpoints.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import pandas as pd
-
-data_path = Path(__file__).parents[1] / "data"
-tox21_presplit_df = pd.read_csv(
-    data_path / "intermediate_data" / "tox21_presplit.tsv", sep="\t"
-)
-print(tox21_presplit_df["endpoint"].unique())
