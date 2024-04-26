49bb35153844dfea8dd861e9aae27ffa7e3fa939
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-04-26 21:14:39 +0200

    add code for final figures and reduce redundant code

---
+++ scripts/06_create_final_figures.py
@@ -0,0 +1,462 @@
+"""Create figures for the uncertainty estimation predictions."""
+
+# pylint: disable=invalid-name
+# pylint: enable=invalid-name  # disable and enable to ignore the file name
+
+
+from itertools import product
+from pathlib import Path
+from typing import Any
+
+import click
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from molpipeline import Pipeline
+from molpipeline.any2mol import SmilesToMol
+from molpipeline.estimators.similarity_transformation import TanimotoToTraining
+from molpipeline.mol2any import MolToMorganFP
+from plot_utils import (
+    get_nx2_figure,
+    get_performance_metrics,
+    load_data,
+    test_set_composition2ax,
+    test_set_nn_similarity2ax,
+)
+from sklearn.calibration import calibration_curve
+
+
+def plot_test_set_composition(
+    data_df_list: list[pd.DataFrame],
+    data_name_list: list[str] | None = None,
+    save_path: Path | str | None = None,
+    figsize: tuple[int, int] | None = None,
+) -> None:
+    """Plot the composition of the test sets from both endpoints.
+
+    X axis: Number of negative compounds
+    Y axis: Number of positive compounds
+
+    Parameters
+    ----------
+    data_df_list : list[pd.DataFrame]
+        Dataframes with the predictions for the endpoints.
+        Each item in the list is a dataframe for an endpoint.
+    data_name_list : list[str] | None, optional (default=None)
+        Names of the endpoints.
+    save_path : Path | str | None, optional (default=None)
+        Path to save the figure.
+    figsize : tuple[int, int] | None, optional (default=None)
+        Figure size.
+    """
+    _, axs, ax_legend = get_nx2_figure(figsize=figsize, share_y=False)
+    legend = None
+    handles = None
+    for i, data_df in enumerate(data_df_list):
+        if data_name_list:
+            axs[i].set_title(data_name_list[i])
+        if i == 0:
+            handles, legend = test_set_composition2ax(data_df, axs[i])
+        else:
+            test_set_composition2ax(data_df, axs[i])
+        axs[i].set_xlabel("Number of negative compounds")
+        axs[i].set_ylabel("")
+    axs[0].set_ylabel("Number of positive compounds")
+    if not handles or not legend:
+        raise ValueError("No handles and labels found.")
+    ax_legend.legend(handles, legend, loc="center", ncol=4)
+    if save_path:
+        save_path = Path(save_path)
+        plt.savefig(save_path / "test_set_composition.png")
+
+
+def get_sim_pipeline() -> Pipeline:
+    """Get the similarity pipeline."""
+    sim_pipeline = Pipeline(
+        [
+            ("smi2mol", SmilesToMol()),
+            ("mol2morgan", MolToMorganFP()),
+            ("sim", TanimotoToTraining()),
+        ]
+    )
+    return sim_pipeline
+
+
+def get_model_order_and_color() -> tuple[list[str], dict[str, str]]:
+    """Get the model order and color mapping.
+
+    Returns
+    -------
+    list[str]
+        Model order.
+    dict[str, str]
+        Model color mapping.
+    """
+    model_order = [
+        "Morgan FP + KNN",
+        "Neural FP + KNN",
+        "Morgan FP + RF",
+        "Neural FP + RF",
+        "Morgan FP + SVC",
+        "Neural FP + SVC",
+        "Chemprop",
+    ]
+    model_color = {}
+    for i, model in enumerate(model_order):
+        if model != "Chemprop":
+            model_color[model] = sns.color_palette("Paired")[i]
+        else:
+            model_color[model] = sns.color_palette("Paired")[i + 1]
+    return model_order, model_color
+
+
+def plot_similarity_to_training(
+    data_df_list: list[pd.DataFrame],
+    save_path: Path,
+    col_tiles: list[str] | None = None,
+    figsize: tuple[int, int] | None = None,
+) -> None:
+    """Plot the similarity of the test set compounds to the training set.
+
+    Parameters
+    ----------
+    data_df_list : list[pd.DataFrame]
+        Dataframes with the predictions for the endpoints.
+        Each item in the list is a dataframe for an endpoint.
+    save_path : Path
+        Path to save the figure.
+    col_tiles : list[str] | None, optional (default=None)
+        Column titles for the figure.
+    figsize : tuple[int, int] | None, optional (default=None)
+        Figure size.
+    """
+    _, axs, ax_legend = get_nx2_figure(figsize=figsize, nrows=1)
+    handles = None
+    labels = None
+    for i, data_df in enumerate(data_df_list):
+        for j, split in enumerate(["Random", "Agglomerative clustering"]):
+            split_df = data_df.loc[data_df["Split strategy"] == split]
+            test_set_nn_similarity2ax(split_df, axs[i], split)
+        if col_tiles:
+            axs[i].set_title(col_tiles[i])
+        if i == 0:
+            handles, labels = axs[i].get_legend_handles_labels()
+    for ax in axs:
+        ax.legend().remove()
+        ax.set_xlabel("Similarity to training set")
+
+    axs[0].set_ylabel("Count")
+    axs[1].set_ylabel("")
+    axs[1].tick_params(axis="y", which="both", labelleft=False)
+    if not handles or not labels:
+        raise ValueError("No handles and labels found.")
+    ax_legend.legend(handles, labels, loc="center", ncol=4)
+
+    plt.savefig(save_path / "similarity_to_training.png")
+
+
+def plot_metrics(
+    data_df_list: list[pd.DataFrame],
+    data_name_list: list[str] | None = None,
+    save_path: Path | str | None = None,
+    figsize: tuple[int, int] | None = None,
+) -> None:
+    """Plot the performance metrics for each model.
+
+    Parameters
+    ----------
+    data_df_list : list[pd.DataFrame]
+        Predictions for the endpoint.
+    data_name_list : list[str] | None
+        Names of the endpoints.
+    save_path : Path | str | None, optional (default=None)
+        Path to save the figure.
+    figsize : tuple[int, int] | None, optional (default=None)
+        Figure size.
+    """
+    model_order, color_dict = get_model_order_and_color()
+    (_, subfig_list), axs, ax_legend = get_nx2_figure(figsize=figsize, nrows=2)
+    handles = None
+    labels = None
+    for i, data_df in enumerate(data_df_list):
+        performance_df = get_performance_metrics(data_df)
+        if data_name_list:
+            subfig_list[i].suptitle(data_name_list[i])
+        for j, split in enumerate(["Random", "Agglomerative clustering"]):
+            ax = axs[i, j]
+            sns.boxplot(
+                data=performance_df.loc[performance_df["split"] == split],
+                x="metric",
+                hue="model",
+                y="Performance",
+                ax=ax,
+                palette=color_dict,
+                hue_order=model_order,
+            )
+            if i == j == 0:
+                handles, labels = ax.get_legend_handles_labels()
+            ax.set_xlabel("")
+            ax.legend().remove()
+            ax.set_title(f"{split} split")
+            if j == 0:
+                ax.set_ylabel("Metric value")
+            else:
+                ax.set_ylabel("")
+
+    if not handles or not labels:
+        raise ValueError("No handles and labels found.")
+    ax_legend.legend(handles, labels, loc="center", ncol=4)
+    if save_path:
+        save_path = Path(save_path)
+        plt.savefig(save_path / "performance_metrics.png")
+
+
+def plot_metrics_alt(
+    data_df_list: list[pd.DataFrame],
+    data_name_list: list[str] | None = None,
+    save_path: Path | str | None = None,
+    figsize: tuple[int, int] | None = None,
+) -> None:
+    """Plot the performance metrics for each model.
+
+    Parameters
+    ----------
+    data_df_list : list[pd.DataFrame]
+        Predictions for the endpoint.
+    data_name_list : list[str] | None
+        Names of the endpoints.
+    save_path : Path | str | None, optional (default=None)
+        Path to save the figure.
+    figsize : tuple[int, int] | None, optional (default=None)
+        Figure size.
+    """
+    model_order, color_dict = get_model_order_and_color()
+    (_, subfig_list), axs, ax_legend = get_nx2_figure(
+        figsize=figsize, nrows=2, share_y=False
+    )
+    handles = None
+    labels = None
+    for i, data_df in enumerate(data_df_list):
+        performance_df = get_performance_metrics(data_df)
+        if data_name_list:
+            subfig_list[i].suptitle(data_name_list[i])
+        for j, metric in enumerate(["BA", "Brier score"]):
+            ax = axs[i, j]
+            sns.boxplot(
+                data=performance_df.loc[performance_df["metric"] == metric],
+                x="split",
+                hue="model",
+                y="Performance",
+                ax=ax,
+                palette=color_dict,
+                hue_order=model_order,
+            )
+            if i == j == 0:
+                handles, labels = ax.get_legend_handles_labels()
+            ax.set_xlabel("")
+            ax.legend().remove()
+            ax.set_title(metric)
+            if j == 0:
+                ax.set_ylabel("Metric value")
+            else:
+                ax.set_ylabel("")
+
+    if not handles or not labels:
+        raise ValueError("No handles and labels found.")
+    ax_legend.legend(handles, labels, loc="center", ncol=4)
+    if save_path:
+        save_path = Path(save_path)
+        plt.savefig(save_path / "performance_metrics_alt.png")
+
+
+def plot_calibration_curves(
+    data_df_list: list[pd.DataFrame],
+    data_name_list: list[str] | None,
+    save_path: Path,
+    **kwargs: Any,
+) -> None:
+    """Plot the calibration curves for each model.
+
+    Parameters
+    ----------
+    data_df_list : list[pd.DataFrame]
+        Predictions for the endpoints.
+    data_name_list : list[str] | None
+        Names of the endpoints.
+    save_path : Path
+        Path to save the figure.
+    **kwargs
+        Additional keyword arguments.
+    """
+    model_color = get_model_order_and_color()[1]
+    (_, subfig_list), axs, ax_legend = get_nx2_figure(
+        figsize=kwargs.get("figsize", None), nrows=2
+    )
+    name2ax_dict = {"Random": 0, "Agglomerative clustering": 1}
+    legend = None
+    handles = None
+    for row, data_df in enumerate(data_df_list):
+        if data_name_list:
+            subfig_list[row].suptitle(data_name_list[row])
+        for split, split_df in data_df.groupby("Split strategy"):
+            col = name2ax_dict[split]
+            ax = axs[row, col]
+            for model, color in model_color.items():
+                model_df = split_df.loc[split_df["Model name"] == model]
+                prob_true, prob_pred = calibration_curve(
+                    model_df["label"], model_df["proba"], n_bins=10, strategy="uniform"
+                )
+
+                ax.plot(prob_pred, prob_true, label=f"{model}", marker=".", color=color)
+                if row == 0 and split == "Random":
+                    legend, handles = ax.get_legend_handles_labels()
+            ax.plot((0, 1), (0, 1), ls="--", color="gray")
+            ax.set_xlim([0, 1])
+            ax.set_ylim([0, 1])
+            ax.set_title(f"{split} split")
+            if row == 1:
+                ax.set_xlabel("Mean predicted probability (Positive class: 1)")
+            else:
+                ax.set_xlabel("")
+            if col == 0:
+                ax.set_ylabel("Fraction of positives (Positive class: 1)")
+            else:
+                ax.set_ylabel("")
+
+    if not legend or not handles:
+        raise ValueError("No legend and handles found.")
+    ax_legend.legend(legend, handles, loc="center", ncol=4)
+    plt.savefig(save_path / "calibration_curves.png")
+
+
+def plot_proba_rf(
+    data_df_list: list[pd.DataFrame],
+    save_path: Path,
+    data_titles: list[str] | None = None,
+    figsize: tuple[int, int] | None = None,
+) -> None:
+    """Plot the probability distribution for RF models with Morgan and Neural fingerprints.
+
+    Parameters
+    ----------
+    data_df_list : list[pd.DataFrame]
+        Predictions for the endpoint.
+    save_path : Path
+        Path to save the figure.
+    data_titles : list[str] | None, optional (default=None)
+        Data titles for the figure.
+    figsize : tuple[int, int] | None, optional (default=None)
+        Figure size.
+    """
+
+    (_, subfig_list), axs, ax_legend = get_nx2_figure(figsize=figsize, nrows=4)
+
+    models = ["Chemprop", "Neural FP + RF"]
+    splits = ["Random", "Agglomerative clustering"]
+    if data_titles:
+        row_titles = [
+            f"{name} - {split} split" for name, split in product(data_titles, splits)
+        ]
+    else:
+        row_titles = None
+    handles = None
+    for i, (data_df, split) in enumerate(product(data_df_list, splits)):
+        if row_titles:
+            subfig_list[i].suptitle(row_titles[i])
+        for j, model in enumerate(models):
+            split_df = data_df.loc[
+                (data_df["Model name"] == model) & (data_df["Split strategy"] == split)
+            ]
+            sns.histplot(
+                data=split_df,
+                x="proba",
+                ax=axs[i, j],
+                label=f"{model}",
+                alpha=0.5,
+                hue="label",
+                bins=np.linspace(0, 1, 20),
+                stat="density",
+                common_norm=False,
+            )
+            axs[i, j].set_title(f"{model} - {split}")
+            if i == 1:
+                axs[i, j].set_xlabel("Predicted probability")
+            else:
+                axs[i, j].set_xlabel("")
+            if j == 0:
+                axs[i, j].set_ylabel("Density")
+            else:
+                axs[i, j].set_ylabel("")
+            axs[i, j].legend().remove()
+            if i == 0 and j == 1:
+                handles, _ = axs[i, j].get_legend_handles_labels()
+    if not handles:
+        raise ValueError("No handles and labels found.")
+    ax_legend.legend(handles, ["Active", "Inactive"], loc="center", ncol=4)
+    plt.savefig(save_path / "proba_distribution_rf.png")
+
+
+@click.command()
+@click.option(
+    "--endpoint_a", type=str, required=True, help="Endpoint to create figures for."
+)
+@click.option(
+    "--endpoint_b", type=str, required=True, help="Endpoint to create figures for."
+)
+def create_figures(endpoint_a: str, endpoint_b: str) -> None:
+    """Create figures for the uncertainty estimation predictions.
+
+    Notes
+    -----
+    The figures are analog to 05_create_figures.py, but contain 2 endpoints.
+
+    Parameters
+    ----------
+    endpoint_a: str
+        Endpoint to create figures for.
+    endpoint_b: str
+        Endpoint to create figures for.
+    """
+    base_path = Path(__file__).parents[1]
+    prediction_folder = base_path / "data" / "intermediate_data" / "model_predictions"
+    data_a_df = load_data(endpoint_a, prediction_folder)
+    data_b_df = load_data(endpoint_b, prediction_folder)
+
+    save_path = base_path / "data" / "figures" / "final_figures"
+    save_path.mkdir(parents=True, exist_ok=True)
+    plot_test_set_composition(
+        [data_a_df, data_b_df],
+        data_name_list=[endpoint_a, endpoint_b],
+        save_path=save_path,
+    )
+    plot_similarity_to_training(
+        [data_a_df, data_b_df],
+        save_path,
+        col_tiles=[endpoint_a, endpoint_b],
+    )
+    plot_metrics(
+        [data_a_df, data_b_df],
+        data_name_list=[endpoint_a, endpoint_b],
+        save_path=save_path,
+    )
+    plot_metrics_alt(
+        [data_a_df, data_b_df],
+        data_name_list=[endpoint_a, endpoint_b],
+        save_path=save_path,
+    )
+    plot_calibration_curves(
+        [data_a_df, data_b_df],
+        data_name_list=[endpoint_a, endpoint_b],
+        save_path=save_path,
+    )
+    plot_proba_rf(
+        [data_a_df, data_b_df],
+        data_titles=[endpoint_a, endpoint_b],
+        save_path=save_path,
+        figsize=(10, 15),
+    )
+
+
+if __name__ == "__main__":
+    create_figures()  # pylint: disable=no-value-for-parameter
---
+++ scripts/plot_utils.py
@@ -0,0 +1,317 @@
+"""Utility functions for plotting the results of the benchmarking."""
+
+from pathlib import Path
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import seaborn as sns
+from matplotlib.figure import SubFigure
+from molpipeline import Pipeline
+from molpipeline.any2mol import SmilesToMol
+from molpipeline.estimators.similarity_transformation import TanimotoToTraining
+from molpipeline.mol2any import MolToMorganFP
+from sklearn.metrics import balanced_accuracy_score, brier_score_loss
+
+
+def get_sim_pipeline() -> Pipeline:
+    """Get the similarity pipeline."""
+    sim_pipeline = Pipeline(
+        [
+            ("smi2mol", SmilesToMol()),
+            ("mol2morgan", MolToMorganFP()),
+            ("sim", TanimotoToTraining()),
+        ],
+        n_jobs=-1,
+    )
+    return sim_pipeline
+
+
+def remove_ax_frame(ax: plt.Axes) -> None:
+    """Remove the frame of the axis.
+
+    Inplace operation.
+
+    Parameters
+    ----------
+    ax : plt.Axes
+        Axis to remove the frame
+    """
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+
+def get_nx2_figure(  # pylint: disable=too-many-variables
+    figsize: tuple[int, int] | None = None, nrows: int = 1, share_y: bool = True
+) -> tuple[tuple[plt.Figure, list[SubFigure]], npt.NDArray[plt.Axes], plt.Axes]:  # type: ignore
+    """Get a figure with n rows, 2 cols, and a legend axis.
+
+    Parameters
+    ----------
+    figsize : tuple[int, int], optional (default=None)
+        Figure size. If None, the default size is (10, 5).
+    nrows : int, optional (default=1)
+        Number of rows.
+    share_y : bool, optional (default=True)
+        Whether to share the y-axis between the two columns.
+
+    Returns
+    -------
+    plt.Figure
+        The figure containing the axes.
+    npt.NDArray[[plt.Axes]
+        Array of axes.
+    plt.Axes
+        The legend axis.
+    """
+    if figsize is None:
+        figsize = (8, 4 * nrows)
+    fig = plt.figure(layout="constrained", figsize=figsize)
+    row_height = 9 * nrows
+    if nrows > 1:
+        row_height += 1
+    gs = plt.GridSpec(row_height, 1, figure=fig)
+    ax_list = []
+    sub_fig_list = []
+    for i in range(nrows):
+        start_row = 9 * i
+        end_row = 9 * (i + 1) - 1
+        sub_fig = fig.add_subfigure(gs[start_row:end_row, :])
+        sub_fig_list.append(sub_fig)
+        sub_spec = sub_fig.add_gridspec(1, 2)
+        ax_0 = sub_fig.add_subplot(sub_spec[0, 0])
+        if share_y:
+            ax_1 = sub_fig.add_subplot(sub_spec[0, 1], sharey=ax_0)
+        else:
+            ax_1 = sub_fig.add_subplot(sub_spec[0, 1])
+        ax_list.append([ax_0, ax_1])
+    ax_legend = fig.add_subplot(gs[-1:, :])
+    remove_ax_frame(ax_legend)
+    return (fig, sub_fig_list), np.array(ax_list).squeeze(), ax_legend
+
+
+def assert_same_smiles_set(data_df1: pd.DataFrame, data_df2: pd.DataFrame) -> None:
+    """Check if the dataframes have the same smiles set.
+
+    This is to ensure that the predictions are for the same compounds.
+
+    Parameters
+    ----------
+    data_df1 : pd.DataFrame
+        First dataframe.
+    data_df2 : pd.DataFrame
+        Second dataframe.
+
+    Raises
+    ------
+
+    """
+    smiles_set1 = set(data_df1["smiles"].tolist())
+    smiles_set2 = set(data_df2["smiles"].tolist())
+
+    if smiles_set1 != smiles_set2:
+        endpoint_name = data_df1["endpoint"].iloc[0]
+        model = data_df1["model"].iloc[0]
+        split_method = data_df1["Split strategy"].iloc[0]
+        trial = data_df1["trial"].iloc[0]
+        raise ValueError(
+            f"The smiles sets for the neural and morgan predictions are not the same"
+            f" for {endpoint_name}, {model}, {split_method}, {trial}."
+        )
+
+
+def load_data(endpoint: str, prediction_folder: Path) -> pd.DataFrame:
+    """Load the data for the endpoint.
+
+    Parameters
+    ----------
+    endpoint : str
+        Endpoint to load data for.
+    prediction_folder : Path
+        Folder with the predictions.
+
+    Returns
+    -------
+    pd.DataFrame
+        Data for the endpoint.
+    """
+    nnfp_prediction_df = pd.read_csv(
+        prediction_folder / f"neural_fingerprint_predictions_{endpoint}.tsv.gz",
+        sep="\t",
+    )
+    nnfp_prediction_df["encoding"] = "Neural FP"
+    morganfp_prediction_df = pd.read_csv(
+        prediction_folder / f"morgan_fingerprint_predictions_{endpoint}.tsv.gz",
+        sep="\t",
+    )
+    morganfp_prediction_df["encoding"] = "Morgan FP"
+    split_columns = ["endpoint", "model", "Split strategy", "trial"]
+    for idx_list, iter_df in nnfp_prediction_df.groupby(split_columns):
+        morgan_iter_df = morganfp_prediction_df
+        for col_name, col_value in zip(split_columns, idx_list):  # type: ignore
+            morgan_iter_df = morgan_iter_df.loc[morgan_iter_df[col_name] == col_value]
+
+        if idx_list[1] != "Chemprop":  # Chemprop has no corresponding Morgan model
+            assert_same_smiles_set(iter_df, morgan_iter_df)
+    endpoint_df = pd.concat([nnfp_prediction_df, morganfp_prediction_df])
+    endpoint_df["Model name"] = endpoint_df[["encoding", "model"]].apply(
+        " + ".join, axis=1
+    )
+    endpoint_df.loc[endpoint_df["model"] == "Chemprop", "Model name"] = "Chemprop"
+    return endpoint_df
+
+
+def get_test_set_compounds(data_df: pd.DataFrame) -> pd.DataFrame:
+    """Get the compounds used as test set.
+
+    Gets
+
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        Data for the endpoint.
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with the test set compounds.
+    """
+    test_set_df = data_df[
+        ["smiles", "label", "trial", "Split strategy"]
+    ].drop_duplicates()
+    for split_stragegy, split_df in test_set_df.groupby("Split strategy"):
+        for trial, test_df in split_df.groupby("trial"):
+            training_df = split_df.loc[split_df["trial"] != trial]
+            training_smiles = training_df["smiles"].tolist()
+            test_smiles = test_df["smiles"].tolist()
+            intersection = set(training_smiles).intersection(set(test_smiles))
+            if intersection:
+                raise ValueError(
+                    f"Test set for {split_stragegy}, {trial} contains compounds "
+                    f"also in the training set."
+                )
+    return test_set_df
+
+
+def test_set_composition2ax(
+    data_df: pd.DataFrame, ax: plt.Axes
+) -> tuple[list[plt.Artist], list[Any]]:
+    """Plot the composition of the test set.
+
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        Data for the endpoint.
+    ax : plt.Axes
+        Axes to plot the composition.
+    """
+    test_set_cpds = get_test_set_compounds(data_df)
+    count_df = test_set_cpds.pivot_table(
+        index=["trial", "Split strategy"],
+        values="smiles",
+        columns="label",
+        aggfunc="nunique",
+    )
+    count_df.reset_index(inplace=True)
+    pos_total, neg_total = count_df.query("`Split strategy` == 'Random'")[[1, 0]].sum(
+        axis=0
+    )
+    n_trials = test_set_cpds.trial.nunique()
+    sns.scatterplot(data=count_df, x=0, y=1, hue="Split strategy", ax=ax)
+    ax.scatter(
+        [neg_total / n_trials],
+        [pos_total / n_trials],
+        label="Perfect split",
+        marker="x",
+        color="k",
+    )
+    radius_10_cpds = plt.Circle(
+        (neg_total / n_trials, pos_total / n_trials), 10, fill=False
+    )
+
+    ax.axis("equal")
+    ax.add_artist(radius_10_cpds)
+    handles, legend = ax.get_legend_handles_labels()
+    ax.legend().remove()
+    x_lim = ax.get_xlim()
+    y_lim = ax.get_ylim()
+    ax.plot([0, 5000], [0, 5000], color="grey", ls="--")
+    ax.set_xlim(x_lim)
+    ax.set_ylim(y_lim)
+    return handles, legend
+
+
+def test_set_nn_similarity2ax(
+    data_df: pd.DataFrame, ax: plt.Axes, label: str | None = None
+) -> tuple[list[plt.Artist], list[Any]]:
+    """Plot the similarity of the test set compounds to the training set.
+
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        Data for the endpoint.
+    ax : plt.Axes
+        Axes to plot the similarity.
+    label : str | None, optional (default=None)
+        Label for the plot.
+    """
+    sim_list = []
+    for trial in data_df.trial.unique():
+        training_smis = data_df.loc[data_df.trial != trial, "smiles"].tolist()
+        test_smis = data_df.loc[data_df.trial == trial, "smiles"].tolist()
+        sim_pl = get_sim_pipeline()
+        sim_pl.fit(training_smis)
+        max_sim = sim_pl.transform(test_smis).max(axis=1)
+        if len(max_sim) != len(test_smis):
+            raise AssertionError()
+        sim_list.extend(max_sim.tolist())
+
+    sns.histplot(sim_list, ax=ax, label=label, bins=np.linspace(0, 1, 20), alpha=0.5)
+    ax.set_xlabel("Similarity to training set")
+    ax.set_ylabel("Count")
+
+    return ax.get_legend_handles_labels()
+
+
+def get_performance_metrics(data_df: pd.DataFrame) -> pd.DataFrame:
+    """Get the performance metrics for each model.
+
+    Parameters
+    ----------
+    data_df : pd.DataFrame
+        Predictions for the endpoint.
+
+    Returns
+    -------
+    pd.DataFrame
+        Performance metrics for each model and split.
+    """
+    performance_df_list = []
+    for (model, trial, split), iter_df in data_df.groupby(
+        ["Model name", "trial", "Split strategy"]
+    ):
+        iter_dict = {
+            "model": model,
+            "trial": trial,
+            "split": split,
+        }
+        ba_dict = {
+            "metric": "BA",
+            "Performance": balanced_accuracy_score(
+                iter_df["label"], iter_df["prediction"]
+            ),
+        }
+        brier_dict = {
+            "metric": "Brier score",
+            "Performance": brier_score_loss(iter_df["label"], iter_df["proba"]),
+        }
+        for perf_dict in [ba_dict, brier_dict]:
+            perf_dict.update(iter_dict)
+            performance_df_list.append(perf_dict)
+    return pd.DataFrame(performance_df_list)
