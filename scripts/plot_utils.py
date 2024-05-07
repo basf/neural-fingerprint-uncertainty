5a493d5b3a268b066e0f0a103175b496e17c5910
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-05-07 22:52:03 +0200

    black

--- scripts/plot_utils.py
+++ scripts/plot_utils.py
@@ -405,7 +405,9 @@ def get_performance_metrics(data_df: pd.DataFrame) -> pd.DataFrame:
         }
         precision_dict = {
             "metric": "Precision",
-            "Performance": precision_score(iter_df["label"], iter_df["prediction"], zero_division=0),
+            "Performance": precision_score(
+                iter_df["label"], iter_df["prediction"], zero_division=0
+            ),
         }
         recall_dict = {
             "metric": "Recall",
