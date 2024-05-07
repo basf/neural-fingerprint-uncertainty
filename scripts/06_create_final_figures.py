d2a731267f548cda25e40ac7c88cb83d227339ea
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-05-07 20:40:57 +0200

    remove unused imports

--- scripts/06_create_final_figures.py
+++ scripts/06_create_final_figures.py
@@ -8,7 +8,6 @@ from itertools import product
 from pathlib import Path
 from typing import Any
 
-import click
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
