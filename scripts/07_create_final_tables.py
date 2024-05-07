b2b179ea4c75317bb2a98c6f18bc46371faa1174
Christian Feldmann <christian-wolfgang.feldmann@basf.com>
2024-05-07 22:38:30 +0200

    simplify code by reusing functions

--- scripts/07_create_final_tables.py
+++ scripts/07_create_final_tables.py
@@ -7,7 +7,7 @@ import numpy.typing as npt
 import pandas as pd
 import yaml
 from loguru import logger
-from plot_utils import get_performance_metrics, load_data
+from plot_utils import get_performance_metrics, load_all_performances, load_data
 
 
 def agg_mean_std(values: npt.NDArray[np.float_]) -> str:
@@ -35,7 +35,6 @@ def create_overview_table(base_path: Path) -> None:
     # Load endpoints from yaml
     with open(config_path, "r", encoding="UTF-8") as file:
         endpoints = yaml.safe_load(file)["endpoint"]
-    performance_df_list = []
     dataset_size_list = []
     for endpoint in endpoints:
         data_df = load_data(endpoint, prediction_folder)
@@ -44,42 +43,20 @@ def create_overview_table(base_path: Path) -> None:
         )
         label_count_df.rename(columns={0: "#Inactives", 1: "#Actives"}, inplace=True)
         dataset_size_list.append(label_count_df)
-        performance_df = get_performance_metrics(data_df)
-        performance_df["endpoint"] = endpoint
-        performance_df_list.append(performance_df)
-    final_performance_df = pd.concat(performance_df_list)
     dataset_size_df = pd.concat(dataset_size_list)
-    final_performance_df = final_performance_df.query("metric == 'Balanced accuracy'")
-    final_performance_df = final_performance_df.query("split == 'Random'")
-    show_model_list = ["Chemprop", "Morgan FP + RF", "Neural FP + RF"]
-    final_performance_df = final_performance_df.loc[
-        final_performance_df["model"].isin(show_model_list)
-    ]
-
-    mean_performance_df = final_performance_df.pivot_table(
-        index="endpoint", columns="model", values="Performance", aggfunc=agg_mean_std
-    )
-    mean_performance_df = dataset_size_df.merge(mean_performance_df, on="endpoint")
-    mean_performance_df.index = mean_performance_df.index.str.replace("_", r"\_")
+    dataset_size_df.index = dataset_size_df.index.str.replace("_", r"\_")
     logger.info("Creating performance table for overview")
-    logger.info(f"\n{mean_performance_df.to_latex()}")
+    logger.info(f"\n{dataset_size_df.to_latex()}")
 
 
 def create_performance_table(base_path: Path) -> None:
     """Create the performance table."""
-    prediction_folder = base_path / "data" / "intermediate_data" / "model_predictions"
-    config_path = base_path / "config" / "endpoints.yaml"
-    # Load endpoints from yaml
-    with open(config_path, "r", encoding="UTF-8") as file:
-        endpoints = yaml.safe_load(file)["endpoint"]
-    performance_df_list = []
-    for endpoint in endpoints:
-        data_df = load_data(endpoint, prediction_folder)
-        performance_df = get_performance_metrics(data_df)
-        performance_df["endpoint"] = endpoint.replace("_", r"\_")
-        performance_df_list.append(performance_df)
-    final_performance_df = pd.concat(performance_df_list)
-    final_performance_df = final_performance_df.query("split == 'Random'")
+    all_data_df = load_all_performances(base_path)
+    all_data_df["endpoint"] = all_data_df["endpoint"].str.replace("_", r"\_")
+    final_performance_df = all_data_df.query("split == 'Random'")
+    final_performance_df = final_performance_df.loc[
+        final_performance_df["metric"].isin(["Balanced accuracy", "Brier score"])
+    ]
     final_performance_df = final_performance_df.loc[
         final_performance_df["model"].isin(
             ["Chemprop", "Morgan FP + RF", "Neural FP + RF"]
